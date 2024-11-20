df2dc242b228f709b7e1e22463a44998f1837806
tots-teo <amarkteo@gmail.com>
2024-11-20 21:11:19 +0800

    Delete users.sql

--- users.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
-);
-
-INSERT INTO users (username, password) VALUES ('testuser', PASSWORD('yourpassword'));
